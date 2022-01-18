e00117d5a1ef4737653e1cb0170d4cb8d833802e
Dante Cesa <dantec@mac.com>
2022-01-18 15:40:34 -0800

    Better iPad support

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -54,7 +54,7 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("BetterRest")
-        }
+        }.navigationViewStyle(StackNavigationViewStyle())
     }
     
     func coffeePrinter() -> String {
